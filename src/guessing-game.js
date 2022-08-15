72f3fdd8337cdf739f85a6698c513e592385b09a
ignatievvv <ramorusbadrobot@gmail.com>
2022-08-15 22:57:27 +0300

    feat: start guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.min = null;
+      this.max = null;
+      this.solution = null;
+    }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      this.solution = Math.round(this.max - (this.max - this.min) / 2);
+      return this.solution;
     }
 
     lower() {
-
+      this.max = this.solution;
+      this.solution = null;
     }
 
     greater() {
-
+      this.min = this.solution;
+      this.solution = null;
     }
 }
 
